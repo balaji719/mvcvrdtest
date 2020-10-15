681e5a2d754194a63832ff748a150e478067f5cf
n balaji <nbalaji313@gmail.com>
2020-10-15 15:11:29 +0530

    Add project files.

---
+++ mvcvrdtest/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+namespace mvcvrdtest.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Models.Employee e1)
+        {
+            // DAL.Employee obj = new DAL.Employee();
+            DAL.Employee1 obj = new DAL.Employee1();
+            int i = obj.AddEmpoyee(e1);
+            if (i == 1)
+            {
+                ViewBag.Message = "Record is inserted";
+            }
+            else
+            {
+                ViewBag.Message = "Failed";
+            }
+            return View();
+        }
+    }
+}
---
+++ mvcvrdtest/DAL/Employee1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace mvcvrdtest.DAL
+{
+    public class Employee1
+    {
+        public int AddEmpoyee(Models.Employee e)
+        {
+            SqlConnection con = new SqlConnection("user id =balaji;password=balaji;database=Balaji;data source=.");
+            con.Open();
+            string query = "insert into Employee values('" + e.Name + "', '" + e.Email + "', '" + e.Salary + "', '" + e.Designaton + "')";
+            SqlCommand sc = new SqlCommand(query, con);
+            int i = sc.ExecuteNonQuery();
+            con.Close();
+            return i;
+
+        }
+
+        public int DeleteEmpoyee(Models.Employee e)
+        {
+            SqlConnection con = new SqlConnection("user id =balaji;password=balaji;database=Balaji;data source=.");
+            con.Open();
+            string query = "delete from emp where eno='" + e.Id + "'')";
+            SqlCommand sc = new SqlCommand(query, con);
+            int i = sc.ExecuteNonQuery();
+            con.Close();
+            return i;
+
+        }
+    }
+}
